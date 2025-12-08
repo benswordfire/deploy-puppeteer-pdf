3c03c268919c7197ad54d3ce889d67bf2e1ae7f8
benswordfire <benswordfire@gmail.com>
2025-12-08 09:41:57 +0100

    even better performance

--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
-import puppeteer, { executablePath } from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import puppeteer from 'puppeteer';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-chromium.setGraphicsMode = false;
+let browserPromise = puppeteer.launch({
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
 
 app.get('/pdf', async(req, res) => {
-  let browser = null;
-
   try {
-    const isProduction = process.env.NODE_ENV === 'production';
-
-    const browserOptions = {
-      args: chromium.args,
-      executablePath: isProduction
-      ? await chromium.executablePath()
-      : process.platform === 'linux'
-        ? '/usr/bin/google-chrome'
-        : undefined,
-      headless: chromium.headless
-    };
-
-    if (!isProduction) {
-      browserOptions.args = [
-        ...browserOptions.args,
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ];
-    }
-
-    browser = await puppeteer.launch(browserOptions);
-
+    const browser = await browserPromise;
     const page = await browser.newPage();
 
     await page.setContent(`<h1>hello, world</h1>`);
-
     const pdfBuffer = await page.pdf();
 
     res.set({ 'Content-Type': 'application/pdf' });
     res.send(pdfBuffer);
+
+    await page.close();
 
   } catch (err) {
     console.error('Failed to generate PDF', err);
@@ -49,10 +29,6 @@ app.get('/pdf', async(req, res) => {
       success: false,
       msg: 'Failed to generate PDF'
     })
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
   }
 });
 
